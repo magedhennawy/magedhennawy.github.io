596222637d0e9a56dff706daad4bcaacf39a3039
Maged Hennawy <magedhennawy@gmail.com>
2019-05-27 22:44:18 -0400

    Update website to reflect changes I want

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,4 @@ import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
 })
 export class AppComponent {
 	@ViewChild("container") container;
-
-	onWhiteChange() {
-		this.container.nativeElement.classList.add("carbon--theme--white");
-		this.container.nativeElement.classList.remove("carbon--theme--g10");
-		this.container.nativeElement.classList.remove("carbon--theme--g100");
-		this.container.nativeElement.classList.remove("carbon--theme--g90");
-	}
-	onG10Change() {
-		this.container.nativeElement.classList.add("carbon--theme--g10");
-		this.container.nativeElement.classList.remove("carbon--theme--white");
-		this.container.nativeElement.classList.remove("carbon--theme--g100");
-		this.container.nativeElement.classList.remove("carbon--theme--g90");
-	}
-	onG100Change() {
-		this.container.nativeElement.classList.add("carbon--theme--g100");
-		this.container.nativeElement.classList.remove("carbon--theme--g10");
-		this.container.nativeElement.classList.remove("carbon--theme--white");
-		this.container.nativeElement.classList.remove("carbon--theme--g90");
-	}
-	onG90Change() {
-		this.container.nativeElement.classList.add("carbon--theme--g90");
-		this.container.nativeElement.classList.remove("carbon--theme--g10");
-		this.container.nativeElement.classList.remove("carbon--theme--g100");
-		this.container.nativeElement.classList.remove("carbon--theme--white");
-	}
 }
